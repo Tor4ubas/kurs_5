0b9e1b061522d751d0c664a88e54157e2abb8de1
Tor4ubas <alex1029@yandex.ru>
2023-10-21 11:37:15 +0300

    Создана функция по выбору 10 понравившихся вакансий

--- main.py
+++ main.py
@@ -19,3 +19,35 @@ def get_user(player: WorkToUser):
 
     player.work_api()
 
+def top_10_vacancies():
+    """
+    Выбирает десять вакансий, которые понравились пользователю
+    """
+    total_vacancies = ReadWriteToJSON.read_json()
+    shuffle(total_vacancies)
+    top_vacancies = []
+    for vacancy in total_vacancies:
+        print(f"\nНазвание компании - {vacancy['company']}"
+              f"\nНазвание вакансии - {vacancy['title']}"
+              f"\nГород - {vacancy['city']}"
+              f"\n{vacancy['salary']}"
+              f"\nТребования: {textwrap.fill(vacancy['requirements'], 75)}")
+        while True:
+            try:
+                choice_user = int(input('\n1 - Да'
+                                        '\n2 - Нет'
+                                        '\nНравится текущая вакансия? '))
+                if choice_user == 1:
+                    top_vacancies.append(vacancy)
+                    break
+                elif choice_user == 2:
+                    break
+                else:
+                    raise ValueError
+            except ValueError:
+                print("Некорректный ввод")
+        if len(top_vacancies) <= 10:
+            continue
+        else:
+            break
+    ReadWriteToJSON.write_json(top_vacancies, file_name='top_vacancies.json',)
