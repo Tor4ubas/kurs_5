214c57ba3dbba205f15b70f9c09832e50ce54a7f
Tor4ubas <alex1029@yandex.ru>
2023-10-21 12:04:37 +0300

    добавлена функция по работе с базой данных

--- main.py
+++ main.py
@@ -52,6 +52,51 @@ def top_10_vacancies():
             break
     ReadWriteToJSON.write_json(top_vacancies, file_name='top_vacancies.json',)
 
+def work_db():
+    """
+    Выполняет работу с базой данных с помощью класса DBManager
+    """
+    work_list = ['Получить список всех компаний и количество вакансий у каждой компании',
+                 'Получить список всех вакансий с указанием названия компании,'
+                 '\nназвания вакансии и зарплаты и ссылки на вакансию',
+                 'Получить среднюю зарплату по вакансиям',
+                 'Получить список всех вакансий, у которых зарплата выше средней по всем вакансиям',
+                 'Получает список всех вакансий, в названии которых содержатся переданные в метод слова,'
+                 '\nнапример "python"',
+                 'Вывести понравившиеся вакансии']
+    while True:
+        try:
+            choice_user = int(input(
+                    f'\n1 - {work_list[0]}'
+                    f'\n2 - {work_list[1]}'
+                    f'\n3 - {work_list[2]}'
+                    f'\n4 - {work_list[3]}'
+                    f'\n5 - {work_list[4]}'
+                    f'\n6 - {work_list[5]}'
+                    f'\n0 - Завершить работу программы'
+                    f'\nВыбирите, что Вы хотите вывести на экран: '))
+            if choice_user in [1, 2, 3, 4, 5, 6]:
+                if choice_user == 1:
+                    DBManager.get_companies_and_vacancies_count('hh_vacancies', 'vacancies')
+                elif choice_user == 2:
+                    user_query = input('Введите название компании: ')
+                    DBManager.get_all_vacancies('hh_vacancies', 'vacancies', user_query)
+                elif choice_user == 3:
+                    DBManager.get_avg_salary('hh_vacancies', 'vacancies')
+                elif choice_user == 4:
+                    DBManager.get_vacancies_with_higher_salary('hh_vacancies', 'vacancies')
+                elif choice_user == 5:
+                    user_query = input('Введите ключевое слово для запроса: ')
+                    DBManager.get_vacancies_with_keyword('hh_vacancies', 'vacancies', user_query)
+                elif choice_user == 6:
+                    DBManager.get_top10_vacancies('hh_vacancies', 'top_vacancies')
+            elif choice_user == 0:
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print("\nНекорректный ввод")
+
 
 def main():
     """
