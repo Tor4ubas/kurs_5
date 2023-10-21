f83a293343ee52a44b804568103fdc7f0a3fe3d9
Tor4ubas <alex1029@yandex.ru>
2023-10-21 13:44:00 +0300

    создан Класс для работы с API HeadHunter

--- src/work_api.py
+++ src/work_api.py
@@ -14,4 +14,14 @@ class WorkApi(ABC):
     def get_info(self):
         pass
 
+class HeadHunter(WorkApi):
+    """Класс для работы с API HeadHunter"""
+
+    url = 'https://api.hh.ru/vacancies'
+
+    def __init__(self, text: str, per_page: int, city: int):
+        self.text = text
+        self.per_page = per_page
+        self.area = city
+
 
