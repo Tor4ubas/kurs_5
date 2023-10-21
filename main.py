aff9e036b1cc89d403b97b2fdbdd01eb10c7efaf
Tor4ubas <alex1029@yandex.ru>
2023-10-21 12:02:23 +0300

    добавлен основной модуль работы программы

--- main.py
+++ main.py
@@ -53,3 +53,46 @@ def top_10_vacancies():
     ReadWriteToJSON.write_json(top_vacancies, file_name='top_vacancies.json',)
 
 
+def main():
+    """
+    Выполняет работу программы
+    """
+
+    while input('Нажмите Enter, чтобы начать: ') != '':
+        continue
+
+    # Очищаем файлы
+    f = open('vacancies.json', 'w')
+    f.close()
+    f = open('top_vacancies.json', 'w')
+    f.close()
+    f = open('queries.sql', 'w')
+    f.close()
+
+    # Создаем базу данных
+    DBManager.create_db('hh_vacancies')
+
+    print('\nПриветствую Вас! Подготовим Ваш запрос по поиску вакансий.')
+
+    player = WorkToUser()
+
+    get_user(player)
+
+    print('\nТеперь выбирем топ 10 вакансий, которые Вам нравятся.')
+    top_10_vacancies()
+
+    # Сохраняем вакансии в базе данных
+    DBManager.save_data_to_database(ReadWriteToJSON.read_json('vacancies.json'), 'hh_vacancies', 'vacancies')
+    DBManager.save_data_to_database(ReadWriteToJSON.read_json('top_vacancies.json'), 'hh_vacancies', 'top_vacancies')
+
+    # Работа с классом DBManager
+    work_db()
+
+    print('\nВсе SQL-запросы Вы можете посмотреть в файле queries.sql')
+    print('\nДо новых встреч!доб')
+
+
+if __name__ == '__main__':
+    main()
+
+
