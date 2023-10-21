397f67182638a5d3bf8a8121aa52a0d58c4d11d3
Tor4ubas <alex1029@yandex.ru>
2023-10-21 13:35:30 +0300

    добавлено получение списка понравившихся вакансий

--- src/db_manager.py
+++ src/db_manager.py
@@ -242,5 +242,31 @@ class DBManager:
         except Exception as error:
             print('Ошибка при работе PostgreSQL', error)
 
+    @staticmethod
+    def get_top10_vacancies(database_name: str, table_name: str, params=config()):
+        """
+        Получает список всех вакансий, которые понравились пользователю
+        """
+        try:
+            connection = psycopg2.connect(dbname=database_name, **params)
+            cursor = connection.cursor()
+            postgresql_select_query = f" select * from {table_name} "
+            ReadWriteToSQL.add_info(f"\nSELECT *"
+                                    f"\nFROM {table_name};\n")
+
+            cursor.execute(postgresql_select_query)
+            total_vacancies = cursor.fetchall()
+
+            for row in total_vacancies:
+                print(f'\nКомпания - {row[1]},'
+                      f'\nВакансия - {row[2]},'
+                      f'\nЗарплата - {row[4]},'
+                      f'\nСсылка на вакансию - {row[5]}')
+
+            cursor.close()
+            connection.close()
+        except Exception as error:
+            print('Ошибка при работе PostgreSQL', error)
+
 
 
